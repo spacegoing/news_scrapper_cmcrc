a9947f43d2b8efc5b0bafdfbadbbbc7afc521c65
spacegoing <spacegoing@gmail.com>
2018-06-07 09:38:50 +0000

    [New] Finished CSV

--- compare_s3/agg_date_news3.py
+++ compare_s3/agg_date_news3.py
@@ -54,3 +54,56 @@ def insert_date_tickers_count(odb_name):
 
 for i in opedia_db.collection_names():
   insert_date_tickers_count(i)
+
+
+mqd_db = client['mqd_daily_info']
+mqd_agg_db = client['mqd_daily_info_agg']
+
+def insert_mqd_date_tickers_count(odb_name):
+  '''
+  query stockopedia_news.Market_news collection
+  aggregate news by date
+  list all tickers have news on that date
+
+  insert date, list of tickers, num of tickers in
+  stockopedia_news_agg.Market_News collection
+  '''
+  ocol = mqd_db[odb_name]
+  agg_col = mqd_agg_db[odb_name]
+  agg_col.drop()
+
+  agg_pipeline = [{
+      "$project": {
+          "datestr": {
+              "$dateToString": {
+                  "format": "%Y-%m-%d",
+                  "date": "$date"
+              }
+          },
+          "ric": 1,
+      }
+  }, {
+      "$group": {
+          "_id": {
+              "datestr": "$datestr",
+          },
+          "ticker_list": {
+              "$addToSet": "$ric"
+          }
+      }
+  }, {
+      "$sort": {
+          "_id": 1
+      }
+  }]
+  agg_crs = ocol.aggregate(agg_pipeline, allowDiskUse=True)
+
+  agg_col.insert_many(({
+      'datestr': i['_id']['datestr'],
+      "ticker_list": i['ticker_list'],
+      "num_news": len(i['ticker_list'])
+  } for i in agg_crs))
+
+
+for i in mqd_db.collection_names():
+  insert_mqd_date_tickers_count(i)
--- compare_s3/compare4.py
+++ compare_s3/compare4.py
@@ -1,12 +1,11 @@
 from pymongo import MongoClient
+import dateparser as dp
+import pandas as pd
 
 client = MongoClient('mongodb://localhost:27017/')
-mqd_db = client['mqd_daily_info']
+mqd_agg_db = client['mqd_daily_info_agg']
 opedia_agg_db = client['stockopedia_news_agg']
-
-mqd_uptick_name_list = [
-    'johannesburg', 'sgx', 'lse', 'sao_paulo', 'nasdaq', 'istanbul', 'asx'
-]
+opedia_news_db = client['stockopedia_news']
 
 # dbname in mqd_daily_info : dbname in stockopedia_news
 mqd_opedia_dict = {
@@ -17,3 +16,52 @@ mqd_opedia_dict = {
     'nasdaq': ["NAQ_News", "NMQ_News", "NSQ_News"]
 }
 
+for mcol, scol_list in mqd_opedia_dict.items():
+  scol = scol_list[0]
+
+  mqd_col = mqd_agg_db[mcol]
+  sto_col = opedia_agg_db[scol]
+
+  date_mnum_snum_covrate = [[
+      'date', 'daily_info_sec', 'scrapper_sec', 'coverage'
+  ]]
+  for i in mqd_col.find():
+    mdate = i['datestr']
+    mlist = i['ticker_list']
+    b = sto_col.find_one({"datestr": {"$eq": mdate}})
+
+    if b:
+      slist = b['ticker_list']
+      date_mnum_snum_covrate.append([
+          mdate, i['num_news'], b['num_news'],
+          len(set(mlist).intersection(slist)) / i['num_news']
+      ])
+
+  pd.DataFrame(date_mnum_snum_covrate).to_csv(
+      mcol + '.csv', header=False, index=False)
+
+
+def mkt_date_secs(mcol, mdate):
+  """
+  print secs for
+  mcol : uptick name for market
+  mdate: date
+
+  lse istanbul isin
+  nasdaq mkts
+  sgx ticker ric
+  asx
+  """
+  scol = mqd_opedia_dict[mcol][0]
+
+  mqd_col = mqd_agg_db[mcol]
+  sto_col = opedia_agg_db[scol]
+
+  i = mqd_col.find_one({"datestr": {"$eq": mdate}})
+  b = sto_col.find_one({"datestr": {"$eq": mdate}})
+
+  slist = b['ticker_list']
+  mlist = i['ticker_list']
+
+  print(slist)
+  print(mlist)
